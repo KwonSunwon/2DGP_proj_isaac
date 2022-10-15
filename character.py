cbf5f552e712448f7220ab960f7debdaf1e8a389
KwonSunwon <kso00011147@gmail.com>
2022-10-16 04:24:34 +0900

    feat: Add move and attack keys
    키 입력에 따른 이동 및 공격 애니메이션 추가

--- character.py
+++ character.py
@@ -1,4 +1,6 @@
+from posixpath import isabs
 from tkinter import Frame
+from turtle import back
 from pico2d import *
 
 from math import pi
@@ -8,11 +10,12 @@ MAX_HP = 24
 X, Y = 0, 1
 CLIP_SIZE = 32
 IMAGE_SIZE = 96
-FRONT, BACK, LEFT, RIGHT = range(4)
+IDLE = 0
+FRONT, BACK, LEFT, RIGHT = 0b0001, 0b0010, 0b0100, 0b1000
 
 MOVE_CLIP_POS = [[0, 448], [32, 448], [64, 448], [96, 448], [128, 448], [160, 448], [192, 448], [224, 448], [192, 480], [224, 480]]
 MOVE_SIDE_CLIP_POS = [[0, 416], [32, 416], [64, 416], [96, 416], [128, 416], [160, 416], [192, 416], [224, 416], [0, 384], [32, 384]]
-HEAD_GAP = 29
+HEAD_GAP = 29 
 
 class Character:
     image = None
@@ -21,8 +24,10 @@ class Character:
         if Character.image == None:
             self.image = load_image('./resources/character.png')
         
-        self.direction_head = LEFT
-        self.direction_body = FRONT
+        self.lookHead = FRONT
+        self.lookBody = FRONT
+        self.directionMove = IDLE
+        self.directionAttack = IDLE
         self.frame = 0
         self.x = 720
         self.y = 408
@@ -33,27 +38,66 @@ class Character:
         pass
 
     def update(self):
-        pass
+        if self.directionMove == IDLE:
+            self.lookBody = FRONT
+        elif self.directionMove & LEFT:
+            if self.directionMove & FRONT:
+                self.lookBody = FRONT
+            elif self.directionMove & BACK:
+                self.lookBody = BACK
+            else:
+                self.lookBody = LEFT
+        elif self.directionMove & RIGHT:
+            if self.directionMove & FRONT:
+                self.lookBody = FRONT
+            elif self.directionMove & BACK:
+                self.lookBody = BACK
+            else:
+                self.lookBody = RIGHT
+        
+        if self.directionAttack == IDLE:
+            self.lookHead = self.lookBody
+        else:
+            if self.directionAttack & LEFT:
+                self.lookHead = LEFT
+            elif self.directionAttack & RIGHT:
+                self.lookHead = RIGHT
+            elif self.directionAttack & FRONT:
+                self.lookHead = FRONT
+            elif self.directionAttack & BACK:
+                self.lookHead = BACK
+        
+        self.frame = (self.frame + 1) % 10
 
     ### Draw Functions ###
     def draw_head(self):
-        if self.direction_head == FRONT:
+        if self.lookHead == FRONT:
             self.image.clip_composite_draw(0, 480, CLIP_SIZE, CLIP_SIZE, 0, '', self.x, self.y + HEAD_GAP, IMAGE_SIZE, IMAGE_SIZE)
-        elif self.direction_head == LEFT:
-            self.image.clip_composite_draw(64, 480, CLIP_SIZE, CLIP_SIZE, pi, 'v', self.x, self.y + HEAD_GAP, IMAGE_SIZE, IMAGE_SIZE)
-        elif self.direction_head == RIGHT:
-            self.image.clip_composite_draw(64, 480, CLIP_SIZE, CLIP_SIZE, 0, '', self.x, self.y + HEAD_GAP, IMAGE_SIZE, IMAGE_SIZE)
-        elif self.direction_head == BACK:
+        elif self.lookHead == BACK:
             self.image.clip_composite_draw(128, 480, CLIP_SIZE, CLIP_SIZE, 0, '', self.x, self.y + HEAD_GAP, IMAGE_SIZE, IMAGE_SIZE)
+        elif self.lookHead == LEFT:
+            self.image.clip_composite_draw(64, 480, CLIP_SIZE, CLIP_SIZE, pi, 'v', self.x, self.y + HEAD_GAP, IMAGE_SIZE, IMAGE_SIZE)
+        elif self.lookHead == RIGHT:
+            self.image.clip_composite_draw(64, 480, CLIP_SIZE, CLIP_SIZE, 0, '', self.x, self.y + HEAD_GAP, IMAGE_SIZE, IMAGE_SIZE)
+            
+        # if self.lookBody == FRONT or self.lookBody == IDLE:
+        #     self.image.clip_composite_draw(0, 480, CLIP_SIZE, CLIP_SIZE, 0, '', self.x, self.y + HEAD_GAP, IMAGE_SIZE, IMAGE_SIZE)
+        # elif self.lookBody == LEFT:
+        #     self.image.clip_composite_draw(64, 480, CLIP_SIZE, CLIP_SIZE, pi, 'v', self.x, self.y + HEAD_GAP, IMAGE_SIZE, IMAGE_SIZE)
+        # elif self.lookBody == RIGHT:
+        #     self.image.clip_composite_draw(64, 480, CLIP_SIZE, CLIP_SIZE, 0, '', self.x, self.y + HEAD_GAP, IMAGE_SIZE, IMAGE_SIZE)
+        # elif self.lookBody == BACK:
+        #     self.image.clip_composite_draw(128, 480, CLIP_SIZE, CLIP_SIZE, 0, '', self.x, self.y + HEAD_GAP, IMAGE_SIZE, IMAGE_SIZE)
     
     def draw_body(self):
-        if self.direction_body == FRONT or self.direction_body == BACK:
+        if self.directionMove == IDLE:
+            self.image.clip_composite_draw(MOVE_CLIP_POS[0][X], MOVE_CLIP_POS[0][Y], CLIP_SIZE, CLIP_SIZE, 0, '', self.x, self.y, IMAGE_SIZE, IMAGE_SIZE)
+        elif self.lookBody == FRONT or self.lookBody == BACK:
             self.image.clip_composite_draw(MOVE_CLIP_POS[self.frame][X], MOVE_CLIP_POS[self.frame][Y], CLIP_SIZE, CLIP_SIZE, 0, '', self.x, self.y, IMAGE_SIZE, IMAGE_SIZE)
-        elif self.direction_body == LEFT:
+        elif self.lookBody == LEFT:
             self.image.clip_composite_draw(MOVE_SIDE_CLIP_POS[self.frame][X], MOVE_SIDE_CLIP_POS[self.frame][Y], CLIP_SIZE, CLIP_SIZE, pi, 'v', self.x, self.y, IMAGE_SIZE, IMAGE_SIZE)
-        elif self.direction_body == RIGHT:
+        elif self.lookBody == RIGHT:
             self.image.clip_composite_draw(MOVE_SIDE_CLIP_POS[self.frame][X], MOVE_SIDE_CLIP_POS[self.frame][Y], CLIP_SIZE, CLIP_SIZE, 0, '', self.x, self.y, IMAGE_SIZE, IMAGE_SIZE)
-            
     ######################
     
     def draw(self):
@@ -61,4 +105,67 @@ class Character:
         self.draw_head()
         
     def handle_event(self, event):
-        pass
+        if event.type == SDL_KEYDOWN:
+            ### Key Down ###
+            match event.key:
+                case pico2d.SDLK_LEFT:
+                    self.directionAttack |= LEFT
+                    return
+                case pico2d.SDLK_RIGHT:
+                    self.directionAttack |= RIGHT
+                    return
+                case pico2d.SDLK_UP:
+                    self.directionAttack |= BACK
+                    return
+                case pico2d.SDLK_DOWN:
+                    self.directionAttack |= FRONT
+                    return
+                
+                case pico2d.SDLK_a:
+                    self.directionMove |= LEFT
+                    # self.lookBody = LEFT
+                    return
+                case pico2d.SDLK_d:
+                    self.directionMove |= RIGHT
+                    # self.lookBody = RIGHT
+                    return
+                case pico2d.SDLK_w:
+                    self.directionMove |= BACK
+                    # self.lookBody = BACK
+                    return
+                case pico2d.SDLK_s:
+                    self.directionMove |= FRONT
+                    # self.lookBody = FRONT
+                    return
+        ### Key Up ###
+        elif event.type == SDL_KEYUP:
+            match event.key:
+                case pico2d.SDLK_LEFT:
+                    # print("test - left")
+                    self.directionAttack &= ~LEFT
+                    return
+                case pico2d.SDLK_RIGHT:
+                    # print("test - right")
+                    self.directionAttack &= ~RIGHT
+                    return
+                case pico2d.SDLK_UP:
+                    # print("test - up")
+                    self.directionAttack &= ~BACK
+                    return
+                case pico2d.SDLK_DOWN:
+                    # print("test - down")
+                    self.directionAttack &= ~FRONT
+                    return
+                
+                case pico2d.SDLK_a:
+                    self.directionMove &= ~LEFT
+                    return
+                case pico2d.SDLK_d:
+                    self.directionMove &= ~RIGHT
+                    return
+                case pico2d.SDLK_w:
+                    self.directionMove &= ~BACK
+                    return
+                case pico2d.SDLK_s:
+                    self.directionMove &= ~FRONT
+                    return
--- main_state.py
+++ main_state.py
@@ -30,11 +30,19 @@ def resume():
 
 def handle_events():
     events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            game_framework.quit()
+        else:
+            character.handle_event(event)
     pass
 
 
 def update():
     ui.update()
+    character.update()
 
 
 def draw():
