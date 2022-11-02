6e086e3bf59964f75593ece88a577e4008adaa64
KwonSunwon <kso00011147@gmail.com>
2022-11-02 17:50:39 +0900

    create enemy class and image init
    일반 적 3가지 타입 클래스 추가 및 이미지 맵핑

--- enemy.py
+++ enemy.py
@@ -18,3 +18,66 @@ class Enemy(Creature):
 
     def handle_event(self, event):
         pass
+
+
+class Fly(Enemy):
+    image = None
+    
+    def __init__(self):
+        if Fly.image == None:
+            self.image = load_image('./resources/monsters/fly.png')
+        pass
+
+    def add_event(self, event):
+        pass
+
+    def update(self):
+        pass
+
+    def draw(self):
+        pass
+
+    def handle_event(self, event):
+        pass
+    
+
+class Meat(Enemy):
+    image = None
+    
+    def __init__(self):
+        if Meat.image == None:
+            self.image = load_image('./resources/monsters/meat.png')
+        pass
+
+    def add_event(self, event):
+        pass
+
+    def update(self):
+        pass
+
+    def draw(self):
+        pass
+
+    def handle_event(self, event):
+        pass
+    
+
+class Charger(Enemy):
+    image = None
+    
+    def __init__(self):
+        if Charger.image == None:
+            self.image = load_image('./resources/monsters/charger.png')
+        pass
+
+    def add_event(self, event):
+        pass
+
+    def update(self):
+        pass
+
+    def draw(self):
+        pass
+
+    def handle_event(self, event):
+        pass
