34bb23633a87ce3092caee2d9007960283d877a6
KwonSunwon <kso00011147@gmail.com>
2022-11-11 02:02:10 +0900

    feat: 움직이지 않는 오브젝트 클래스 생성

---
+++ static.py
@@ -0,0 +1,75 @@
+import pico2d
+import game_framework
+import game_world
+
+class Static:
+    x, y = 0, 0
+    WIDTH, HEIGHT = 96, 96
+    
+    def __init__(self):    
+        pass
+
+    def add_event(self, event):
+        pass
+
+    def update(self):
+        pass
+
+    def draw(self):
+        pass
+
+    def handle_event(self, event):
+        pass
+
+    def get_bb(self):
+        return self.x - self.WIDTH // 2, self.y - self.HEIGHT // 2, self.x + self.WIDTH // 2, self.y + self.HEIGHT // 2
+    
+    def handle_collision(self, other, group):
+        pass
+    
+
+class Rock(Static):
+    image = None
+    
+    def __init__(self):
+        if self.image == None:
+            self.image = pico2d.load_image('resource\\image\\rock.png')
+        
+    pass
+
+
+class Spike(Static):
+    image = None
+    
+    def __init__(self):
+        if self.image == None:
+            self.image = pico2d.load_image('resource\\image\\spike.png')
+    pass
+
+
+class Poop(Static):
+    image = None
+    
+    def __init__(self):
+        if self.image == None:
+            self.image = pico2d.load_image('resource\\image\\spike.png')
+    pass
+
+
+class Door(Static):
+    image = None
+    
+    
+    def __init__(self):
+        if self.image == None:
+            self.image = pico2d.load_image('resource\\image\\spike.png')
+    pass
+
+
+class Pit(Static):
+    image = None
+    
+    def __init__(self):
+        if self.image == None:
+            self.image = pico2d.load_image('resource\\image\\spike.png')
+    pass
