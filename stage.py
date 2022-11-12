2a69ccf9022554d5099dc58a9fde04f27443f449
KwonSunwon <kso00011147@gmail.com>
2022-11-12 16:57:38 +0900

    스테이지 출력 클래스 분리

--- stage.py
+++ stage.py
@@ -1,8 +1,11 @@
 from cmath import pi
-from logging import root
 from pico2d import *
+import game_world
 
 import player
+
+import room_type
+import static
 
 width = 1440
 height = 864
@@ -17,46 +20,23 @@ WALL = 20
 DOOR_NORMAL = 21
 DOOR_TREASURE = 22
 DOOR_BOSS = 23
-
-
 DOOR_TRAP = 24
 
-# start room
-room_type_01 = [[WALL, WALL, WALL, WALL, WALL, WALL, WALL, DOOR_NORMAL, WALL, WALL,WALL, WALL, WALL, WALL, WALL],
-                [WALL, None, None, None, None, None, None, None, None, None,None, None, None, None, WALL],
-                [WALL, None, None, None, None, None, None, None, None, None,None, None, None, None, WALL],
-                [WALL, None, None, None, None, None, None, None, None, None,None, None, None, None, WALL],
-                [DOOR_NORMAL, None, None, None, None, None, None, None, None, None,None, None, None, WALL, DOOR_BOSS],
-                [WALL, None, None, None, None, None, None, None, None, None,None, None, None, None, WALL],
-                [WALL, None, None, None, None, None, None, None, None, None,None, None, None, None, WALL],
-                [WALL, None, None, None, None, None, None, None, None, None,None, None, None, None, WALL],
-                [WALL, WALL, WALL, WALL, WALL, WALL, WALL, DOOR_TREASURE, WALL, WALL,WALL, WALL, WALL, WALL, WALL]]
+ROCK = 30
+JAR = 31
+SPIKE = 32
+POOP = 33
 
 
 class Room:
-    room_background_image = [None, None]
-    door_normal = None
-    door_treasure = None
-    door_boss = None
-    door_trap = None
+    background_image = None
     
     def __init__(self):
-        if Room.room_background_image[BASEMENT] == None:
-            Room.room_background_image[BASEMENT] = load_image('./resources/BG/basement_bg.png')
-        if Room.room_background_image[CAVES] == None:
-            Room.room_background_image[CAVES] = load_image('./resources/BG/02_caves.png')
-        if Room.door_normal == None:
-            Room.door_normal = load_image('./resources/grid/door_01_normaldoor.png')
-        if Room.door_treasure == None:
-            Room.door_treasure = load_image('./resources/grid/door_02_treasureroomdoor.png')
-        if Room.door_boss == None:
-            Room.door_boss = load_image('./resources/grid/door_03_bossroomdoor.png')
-        if Room.door_trap == None:
-            Room.door_trap = load_image('./resources/grid/door_04_trapdoor.png')
-            
-        self.background = None
+        if Room.background_image == None:
+            Room.background_image = load_image('resources/BG/basement_bg.png')
         self.grid = [[None] * 15 for i in range(9)]
         self.door_state = [None, None, None, None]
+        self.objects = []
         
     
     def add_event(self, event):
@@ -66,50 +46,34 @@ class Room:
         pass
     
     def set_room(self, room_type):
-        self.background = BASEMENT
         for y in range(9):
             for x in range(15):
-                self.grid[y][x] = room_type[y][x]
-            
-    def draw_door(self, door, x, y, direction):
-        if direction == NORTH:
-            door.clip_composite_draw(0, 144, 64, 48, 0, '', 7 * 96 + 48, 8 * 96, 192, 128)
-            door.clip_composite_draw(64, 144, 64, 48, 0, '', 7 * 96 + 48, 8 * 96, 192, 128)
-            door.clip_composite_draw(0, 192, 64, 48, 0, '', 7 * 96 + 48, 8 * 96, 192, 128)
-        elif direction == WEST:
-            door.clip_composite_draw(0, 144, 64, 48, pi / 2, 'w', 96 + 16, 4 * 96 + 48, 192, 144)
-            door.clip_composite_draw(64, 144, 64, 48, pi / 2, 'w', 96 + 16, 4 * 96 + 48, 192, 144)
-            door.clip_composite_draw(0, 192, 64, 48, pi / 2, 'w', 96 + 16, 4 * 96 + 48, 192, 144)
-        elif direction == SOUTH:
-            door.clip_composite_draw(0, 144, 64, 48, -pi / 2, 'h', 14 * 96 - 16, 4 * 96 + 48, 192, 144)
-            door.clip_composite_draw(64, 144, 64, 48, -pi / 2, 'h', 14 * 96 - 16, 4 * 96 + 48, 192, 144)
-            door.clip_composite_draw(0, 192, 64, 48, -pi / 2, 'h', 14 * 96 - 16, 4 * 96 + 48, 192, 144)
-        elif direction == EAST:
-            door.clip_composite_draw(0, 144, 64, 48, pi, 'h', 7 * 96 + 48, 96, 192, 128)
-            door.clip_composite_draw(64, 144, 64, 48, pi, 'h', 7 * 96 + 48, 96, 192, 128)
-            door.clip_composite_draw(0, 192, 64, 48, pi, 'h', 7 * 96 + 48, 96, 192, 128)
+                # self.grid[y][x] = room_type[y][x]
+                # print(room_type[y][x])
+                if room_type[y][x] == WALL:
+                    self.objects.append(static.Wall(x, y))
+                elif room_type[y][x] == DOOR_NORMAL:
+                    self.objects.append(static.Door(x, y, DOOR_NORMAL))
+                elif room_type[y][x] == DOOR_TREASURE:
+                    self.objects.append(static.Door(x, y, DOOR_TREASURE))
+                elif room_type[y][x] == DOOR_BOSS:
+                    self.objects.append(static.Door(x, y, DOOR_BOSS))
+                elif room_type[y][x] == DOOR_TRAP:
+                    self.objects.append(static.Door(x, y, DOOR_TRAP))
+                elif room_type[y][x] == ROCK:
+                    self.objects.append(static.Rock(x, y, ROCK))
+                elif room_type[y][x] == JAR:
+                    self.objects.append(static.Rock(x, y, JAR))
+                elif room_type[y][x] == SPIKE:
+                    self.objects.append(static.Spike(x, y))
+                elif room_type[y][x] == POOP:
+                    self.objects.append(static.Poop(x, y))
+                    
+        game_world.add_objects(self.objects, 1)
 
             
     def draw_grid(self):
-        self.room_background_image[self.background].draw(width / 2, height / 2, 1440, 864)
-        for y in range(9):
-            for x in range(15):
-                if self.grid[y][x] == DOOR_NORMAL:
-                    door = Room.door_normal
-                elif self.grid[y][x] == DOOR_TREASURE:
-                    door = Room.door_treasure
-                elif self.grid[y][x] == DOOR_BOSS:
-                    door = Room.door_boss
-                    
-                if self.grid[y][x] == DOOR_NORMAL or self.grid[y][x] == DOOR_TREASURE or self.grid[y][x] == DOOR_BOSS:
-                    if y == 0 and x == 7: # north
-                        self.draw_door(door, x, y, NORTH)
-                    elif y == 4 and x == 0: # west
-                        self.draw_door(door, x, y, WEST)
-                    elif y == 4 and x == 14: # east
-                        self.draw_door(door, x, y, EAST)
-                    elif y == 8 and x == 7: # south
-                        self.draw_door(door, x, y, SOUTH)
+        self.background_image.draw(width / 2, height / 2, width, height)
     
     def draw(self):
         self.draw_grid();
@@ -139,7 +103,7 @@ class Stage:
     
     def set_stage(self):
         self.stage[4][4] = Room()
-        self.stage[4][4].set_room(room_type_01)
+        self.stage[4][4].set_room(room_type.type_02)
         
 
     def get_state(self):
--- static.py
+++ static.py
@@ -1,12 +1,20 @@
-import pico2d
+from pico2d import *
 import game_framework
 import game_world
 
+import random
+
+from cmath import pi
+
+CLIP_SIZE = 32
+
 class Static:
     x, y = 0, 0
-    WIDTH, HEIGHT = 96, 96
+    WIDTH, HEIGHT = 87, 85
     
-    def __init__(self):    
+    def __init__(self, x, y):
+        self.x = x * self.WIDTH + 64
+        self.y = (8 - y) * self.HEIGHT + 48
         pass
 
     def add_event(self, event):
@@ -16,13 +24,14 @@ class Static:
         pass
 
     def draw(self):
+        draw_rectangle(*self.get_bb())
         pass
 
     def handle_event(self, event):
         pass
 
     def get_bb(self):
-        return self.x - self.WIDTH // 2, self.y - self.HEIGHT // 2, self.x + self.WIDTH // 2, self.y + self.HEIGHT // 2
+        return self.x, self.y, self.x + self.WIDTH, self.y + self.HEIGHT
     
     def handle_collision(self, other, group):
         pass
@@ -34,45 +43,118 @@ class Wall(Static):
 class Rock(Static):
     image = None
     
-    def __init__(self):
+    def __init__(self, x, y, type):
         if Rock.image == None:
-            Rock.image = pico2d.load_image('resource\\image\\rock.png')
+            Rock.image = pico2d.load_image('resources/objects/rock.png')
+        super().__init__(x, y)
+        self.type = type
+    
+        self.rock_type = random.randint(0, 2)
+        self.jar_type = random.randint(0, 2)
         
+        # print(self.x, self.y)
+        
+    def draw(self):
+        if self.type == 30:
+            if self.rock_type == 0:
+                self.image.clip_draw(0, 224, CLIP_SIZE, CLIP_SIZE, self.x + self.WIDTH//2, self.y + self.WIDTH//2, self.WIDTH + 4, self.HEIGHT + 4)
+            elif self.rock_type == 1:
+                self.image.clip_draw(32, 224, CLIP_SIZE, CLIP_SIZE, self.x + self.WIDTH//2, self.y + self.WIDTH//2, self.WIDTH + 4, self.HEIGHT + 4)
+            elif self.rock_type == 2:
+                self.image.clip_draw(64, 224, CLIP_SIZE, CLIP_SIZE, self.x + self.WIDTH//2, self.y + self.WIDTH//2, self.WIDTH + 4, self.HEIGHT + 4)
+        elif self.type == 31:
+            if self.rock_type == 0:
+                self.image.clip_draw(64, 192, CLIP_SIZE, CLIP_SIZE, self.x + self.WIDTH//2, self.y + self.WIDTH//2, self.WIDTH + 4, self.HEIGHT + 4)
+            elif self.rock_type == 1:
+                self.image.clip_draw(64, 160, CLIP_SIZE, CLIP_SIZE, self.x + self.WIDTH//2, self.y + self.WIDTH//2, self.WIDTH + 4, self.HEIGHT + 4)
+            elif self.rock_type == 2:
+                self.image.clip_draw(64, 128, CLIP_SIZE, CLIP_SIZE, self.x + self.WIDTH//2, self.y + self.WIDTH//2, self.WIDTH + 4, self.HEIGHT + 4)
+        
+        draw_rectangle(*self.get_bb())
     pass
 
 
 class Spike(Static):
     image = None
     
-    def __init__(self):
+    def __init__(self, x, y):
         if Spike.image == None:
-            Spike.image = pico2d.load_image('resource\\image\\spike.png')
+            Spike.image = pico2d.load_image('resources/objects/spike.png')
+        super().__init__(x, y)
     pass
 
 
 class Poop(Static):
     image = None
     
-    def __init__(self):
+    def __init__(self, x, y):
         if Poop.image == None:
-            Poop.image = pico2d.load_image('resource\\image\\spike.png')
+            Poop.image = pico2d.load_image('resources/objects/poop.png')
+        super().__init__(x, y)
     pass
 
+# Normal Door 21
+# Treasure Door 22
+# Boss Door 23
 
 class Door(Static):
-    image = None
+    normal_image = None
+    treasure_image = None
+    boss_image = None
     
-    
-    def __init__(self):
-        if Door.image == None:
-            Door.image = pico2d.load_image('resource\\image\\spike.png')
-    pass
-
-
-class Pit(Static):
-    image = None
-    
-    def __init__(self):
-        if Pit.image == None:
-            Pit.image = pico2d.load_image('resource\\image\\spike.png')
+    def __init__(self, x, y, type):
+        if Door.normal_image == None:
+            Door.normal_image = pico2d.load_image('resources/objects/normal_door.png')
+        if Door.treasure_image == None:
+            Door.treasure_image = pico2d.load_image('resources/objects/treasure_door.png')
+        if Door.boss_image == None:
+            Door.boss_image = pico2d.load_image('resources/objects/boss_door.png')
+        self.x = x
+        self.y = y
+        
+        if self.y == 0 and self.x == 7: # north
+            self.direction = 0
+        elif self.y == 4 and self.x == 0: # west
+            self.direction = 1
+        elif self.y == 4 and self.x == 14: # east
+            self.direction = 2
+        elif self.y == 8 and self.x == 7: # south
+            self.direction = 3
+        
+        self.type = type
+        
+    def draw(self):
+        if self.type == 21:
+            door = Door.normal_image
+        elif self.type == 22:
+            door = Door.treasure_image
+        elif self.type == 23:
+            door = Door.boss_image
+        
+        if self.direction == 0:
+            door.clip_composite_draw(0, 144, 64, 48, 0, '', 7 * 96 + 48, 8 * 96, 192, 128)
+            door.clip_composite_draw(64, 144, 64, 48, 0, '', 7 * 96 + 48, 8 * 96, 192, 128)
+            door.clip_composite_draw(0, 192, 64, 48, 0, '', 7 * 96 + 48, 8 * 96, 192, 128)
+        elif self.direction == 1:
+            door.clip_composite_draw(0, 144, 64, 48, pi / 2, 'w', 96 + 16, 4 * 96 + 48, 192, 144)
+            door.clip_composite_draw(64, 144, 64, 48, pi / 2, 'w', 96 + 16, 4 * 96 + 48, 192, 144)
+            door.clip_composite_draw(0, 192, 64, 48, pi / 2, 'w', 96 + 16, 4 * 96 + 48, 192, 144)
+        elif self.direction == 2:
+            door.clip_composite_draw(0, 144, 64, 48, -pi / 2, 'h', 14 * 96 - 16, 4 * 96 + 48, 192, 144)
+            door.clip_composite_draw(64, 144, 64, 48, -pi / 2, 'h', 14 * 96 - 16, 4 * 96 + 48, 192, 144)
+            door.clip_composite_draw(0, 192, 64, 48, -pi / 2, 'h', 14 * 96 - 16, 4 * 96 + 48, 192, 144)
+        elif self.direction == 3:
+            door.clip_composite_draw(0, 144, 64, 48, pi, 'h', 7 * 96 + 48, 96, 192, 128)
+            door.clip_composite_draw(64, 144, 64, 48, pi, 'h', 7 * 96 + 48, 96, 192, 128)
+            door.clip_composite_draw(0, 192, 64, 48, pi, 'h', 7 * 96 + 48, 96, 192, 128)
+            
+    def get_bb(self):
+        if self.direction == 0:
+            return 7 * 96 + 48, 8 * 96, 192, 128
+        elif self.direction == 1:
+            return 96 + 16, 4 * 96 + 48, 192, 144
+        elif self.direction == 2:
+            return 14 * 96 - 16, 4 * 96 + 48, 192, 144
+        elif self.direction == 3:
+            return 7 * 96 + 48, 96, 192, 128
     pass
