67bea215fd81607260bf5eaa16281d4f04e69ddb
KwonSunwon <kso00011147@gmail.com>
2022-11-19 15:07:58 +0900

    방 종류 추가

--- room_type.py
+++ room_type.py
@@ -11,16 +11,17 @@ JAR = 31
 SPIKE = 32
 POOP = 33
 
+# Room types
 # start room
-type_01 = [[WALL, WALL, WALL, WALL, WALL, WALL, WALL, DOOR_NORMAL, WALL, WALL,WALL, WALL, WALL, WALL, WALL],
+start = [[WALL, WALL, WALL, WALL, WALL, WALL, WALL, DOOR_NORMAL, WALL, WALL,WALL, WALL, WALL, WALL, WALL],
             [WALL, None, None, None, None, None, None, None, None, None,None, None, None, None, WALL],
             [WALL, None, None, None, None, None, None, None, None, None,None, None, None, None, WALL],
             [WALL, None, None, None, None, None, None, None, None, None,None, None, None, None, WALL],
-            [DOOR_NORMAL, None, None, None, None, None, None, None, None, None,None, None, None, None, DOOR_BOSS],
             [WALL, None, None, None, None, None, None, None, None, None,None, None, None, None, WALL],
             [WALL, None, None, None, None, None, None, None, None, None,None, None, None, None, WALL],
             [WALL, None, None, None, None, None, None, None, None, None,None, None, None, None, WALL],
-            [WALL, WALL, WALL, WALL, WALL, WALL, WALL, DOOR_TREASURE, WALL, WALL,WALL, WALL, WALL, WALL, WALL]]
+            [WALL, None, None, None, None, None, None, None, None, None,None, None, None, None, WALL],
+            [WALL, WALL, WALL, WALL, WALL, WALL, WALL, WALL, WALL, WALL,WALL, WALL, WALL, WALL, WALL]]
 
 type_02 = [[WALL, WALL, WALL, WALL, WALL, WALL, WALL, DOOR_NORMAL, WALL, WALL,WALL, WALL, WALL, WALL, WALL],
             [WALL, None, None, None, None, None, None, None, None, None,None, None, None, None, WALL],
@@ -51,3 +52,50 @@ type_04 = [[WALL, WALL, WALL, WALL, WALL, WALL, WALL, DOOR_NORMAL, WALL, WALL,WA
             [WALL, ROCK, ROCK, ROCK, None, None, None, None, None, None,None, ROCK, ROCK, ROCK, WALL],
             [WALL, None, None, None, None, None, None, None, None, None,None, None, None, None, WALL],
             [WALL, WALL, WALL, WALL, WALL, WALL, WALL, DOOR_NORMAL, WALL, WALL,WALL, WALL, WALL, WALL, WALL]]
+
+type_05 = [[WALL, WALL, WALL, WALL, WALL, WALL, WALL, DOOR_BOSS, WALL, WALL,WALL, WALL, WALL, WALL, WALL],
+            [WALL, None, None, None, None, None, None, None, None, None,None, None, None, None, WALL],
+            [WALL, None, CHARGER, None, None, None, None, None, None, None,None, None, CHARGER, None, WALL],
+            [WALL, None, None, None, None, None, None, ROCK, None, None,None, None, None, None, WALL],
+            [WALL, None, None, None, None, ROCK, ROCK, ROCK, ROCK, ROCK,None, None, None, None, WALL],
+            [WALL, None, None, None, None, None, None, ROCK, None, None,None, None, None, None, WALL],
+            [WALL, None, CHARGER, None, None, None, None, None, None, None,None, None, CHARGER, None, WALL],
+            [WALL, None, None, None, None, None, None, None, None, None,None, None, None, None, WALL],
+            [WALL, WALL, WALL, WALL, WALL, WALL, WALL, DOOR_NORMAL, WALL, WALL,WALL, WALL, WALL, WALL, WALL]]
+
+type_06 = [[WALL, WALL, WALL, WALL, WALL, WALL, WALL, DOOR_NORMAL, WALL, WALL,WALL, WALL, WALL, WALL, WALL],
+            [WALL, None, None, None, None, None, None, None, None, None,None, None, None, None, WALL],
+            [WALL, None, FLY, FLY, None, None, None, None, None, None,None, FLY, FLY, None, WALL],
+            [WALL, None, FLY, FLY, None, None, None, None, None, None,None, FLY, FLY, None, WALL],
+            [WALL, None, None, None, None, None, None, None, None, None,None, None, None, None, WALL],
+            [WALL, None, None, None, None, None, None, None, None, None,None, None, None, None, WALL],
+            [WALL, None, None, None, None, None, None, None, None, None,None, None, None, None, WALL],
+            [WALL, None, None, None, None, None, None, None, None, None,None, None, None, None, WALL],
+            [WALL, WALL, WALL, WALL, WALL, WALL, WALL, DOOR_NORMAL, WALL, WALL,WALL, WALL, WALL, WALL, WALL]]
+
+type_07 = [[WALL, WALL, WALL, WALL, WALL, WALL, WALL, DOOR_NORMAL, WALL, WALL,WALL, WALL, WALL, WALL, WALL],
+            [WALL, JAR, JAR, None, None, None, None, None, None, None,None, None, JAR, JAR, WALL],
+            [WALL, JAR, JAR, None, None, None, None, None, None, None,None, None, JAR, JAR, WALL],
+            [WALL, JAR, JAR, None, None, None, None, None, None, None,None, None, JAR, JAR, WALL],
+            [WALL, JAR, JAR, None, None, None, None, None, None, None,None, None, JAR, JAR, WALL],
+            [WALL, JAR, JAR, None, None, None, None, None, None, None,None, None, JAR, JAR, WALL],
+            [WALL, JAR, JAR, None, None, None, None, None, None, None,None, None, JAR, JAR, WALL],
+            [WALL, JAR, JAR, None, None, None, None, None, None, None,None, None, JAR, JAR, WALL],
+            [WALL, WALL, WALL, WALL, WALL, WALL, WALL, DOOR_NORMAL, WALL, WALL,WALL, WALL, WALL, WALL, WALL]]
+
+type_boss = [[WALL, WALL, WALL, WALL, WALL, WALL, WALL, WALL, WALL, WALL,WALL, WALL, WALL, WALL, WALL],
+            [WALL, None, None, None, None, None, None, None, None, None,None, None, None, None, WALL],
+            [WALL, None, None, None, None, None, None, None, None, None,None, None, None, None, WALL],
+            [WALL, None, None, None, None, None, None, None, None, None,None, None, None, None, WALL],
+            [WALL, None, None, None, None, None, None, None, None, None,None, None, None, None, WALL],
+            [WALL, None, None, None, None, None, None, None, None, None,None, None, None, None, WALL],
+            [WALL, None, None, None, None, None, None, None, None, None,None, None, None, None, WALL],
+            [WALL, None, None, None, None, None, None, None, None, None,None, None, None, None, WALL],
+            [WALL, WALL, WALL, WALL, WALL, WALL, WALL, DOOR_BOSS, WALL, WALL,WALL, WALL, WALL, WALL, WALL]]
+
+# Stage 1
+stage_01 = [[None, None, type_boss, None, None],
+            [None, None, type_05, None, None],
+            [None, None, type_07, None, None],
+            [None, None, type_06, None, None],
+            [None, None, start, None, None]]
