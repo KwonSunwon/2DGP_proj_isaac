5ffd23e4216764c32aa0ce8d479ed434bd6f9fbb
KwonSunwon <kso00011147@gmail.com>
2022-12-04 21:22:55 +0900

    feat: plum 공격 패턴 2 이미지 좌표 설정

--- boss.py
+++ boss.py
@@ -49,19 +49,28 @@ class BabyPlum(Enemy):
     
     ATTACK_LIST = ['pattern1', 'pattern2']
     
-    IDLE = ([0, 256], [256, 320])
-    PATTERN1 = ([0, 256], [256, 320], [0, 128], [64, 128], [0, 128],
-                [64, 128], [64, 128], [128, 128], [192, 128], [256, 128],
-                [320, 128], [320, 128], [0, 64], [0, 64], [64, 64],
-                [64, 64], [128, 64], [192, 64], [256, 64], [192, 64],
-                [256, 64], [192, 64], [256, 64], [192, 64], [256, 64],
-                [192, 64], [256, 64], [192, 64], [256, 64], [192, 64],
-                [0, 256], [256, 320], [0, 256])
-    PATTERN2 = ()
+    # Crop X, Y, Scale X, Y, Position X, Y
+    IDLE = ([0, 256, 1, 1, 0, 0], [256, 320, 1, 1, 0, 0])
+    PATTERN1 = ([0, 256, 1, 1, 0, 0], [256, 320, 1.05, 0.95, 0, 0], [0, 128, 1.1, 0.9, 0, 0], [64, 128, 1.13, 0.87, 0, 0], [0, 128, 1.15, 0.85, 0, 0],
+                [64, 128, 1.15, 0.85, 0, 0], [64, 128, 1.12, 0.88, 0, 0], [128, 128, 1.05, 0.95, 0, 0], [192, 128, 1, 1, 0, 0], [256, 128, 0.95, 1.05, 0, 0],
+                [320, 128, 0.9, 1.1, 0, 0], [320, 128, 0.9, 1.1, 0, 0], [0, 64, 0.95, 1.05, 0, 0], [0, 64, 0.95, 1.05, 0, 0], [64, 64, 1.1, 0.9, 0, 0],
+                [64, 64, 1.1, 0.9, 0, 0], [128, 64, 1.15, 0.85, 0, 0], [192, 64, 1, 1, 0, 0], [256, 64, 0.85, 1.15, 0, 0], [192, 64, 0.98, 1.02, 0, 0],
+                [256, 64, 1.1, 0.9, 0, 0], [192, 64, 1.02, 0.98, 0, 0], [256, 64, 0.95, 1.05, 0, 0], [192, 64, 0.97, 1.03, 0, 0], [256, 64, 1, 1, 0, 0],
+                [192, 64, 1, 1, 0, 0], [256, 64, 1, 1, 0, 0], [192, 64, 0.95, 1.05, 0, 0], [256, 64, 0.90, 1.1, 0, 0], [192, 64, 1, 1, 0, 0],
+                [0, 256, 1.1, 0.9, 0, 0], [256, 320, 1.05, 0.95, 0, 0], [0, 256, 0.98, 1.02, 0, 0])
+    
+    PATTERN2 = ([0, 256, 1, 1, 0, 0], [256, 320, 1.05, 0.95, 0, -1], [0, 256, 1.1, 0.9, 0, -2], [256, 320, 1.13, 0.87, 0, -3], [0, 256, 1.15, 0.85, 0, -4],
+                [256, 320, 1, 1, 0, 2], [320, 64, 0.8, 1.2, 0, 7], [384, 64, 0.95, 1.14, 0, 12], [320, 64, 1.1, 0.9, 0, 17], [384, 64, 1.2, 0.8, 0, 16],
+                [320, 64, 0.95, 1.05, 0, 16], [384, 64, 0.98, 1.02, 0, 16], [320, 64, 1, 1, 0, 17], [384, 64, 1, 1, 0, 18], [320, 64, 1, 1, 0, 18],
+                [384, 64, 0.9, 1.1, 0, 18], [320, 64, 0.8, 1.2, 0, 17], [384, 64, 0.7, 1.3, 0, 17], [320, 64, 0.6, 1.4, 0, -6], [448, 64, 0.9, 1.1, -2, -28],
+                [448, 64, 1.15, 0.85, 2, -28], [448, 64, 1.2, 0.8, -2, -28], [448, 64, 1.22, 0.78, 2, -28], [448, 64, 1.23, 0.77, -2, -28], [448, 64, 1.24, 0.76, 1, -28],
+                [448, 64, 1.25, 0.75, -1, -28], [448, 64, 1.26, 0.74, 1, -28], [448, 64, 1.26, 0.74, -1, -28], [448, 64, 1.27, 0.73, 0, -28], [448, 64, 1, 1, 0, -28],
+                [0, 384, 0.8, 1.2, 0, -6], [0, 384, 0.95, 1.05, 0, 1], [0, 384, 1.1, 0.9, 0, 8], [64, 384, 1, 1, 0, 8], [64, 384, 0.9, 1.1, 0, 7],
+                [0, 384, 0.98, 1.02, 0, 2], [0, 256, 1.05, 0.95, 0, -3], [256, 320, 1.02, 0.98, 0, 1], [0, 256, 1, 1, 0, 0], [256, 320, 1, 1, 0, 0])
     
     CLIP = {'idle' : IDLE, 'pattern1' : PATTERN1, 'pattern2' : PATTERN2}
-    FPA = {'idle' : 2, 'pattern1' : 33, 'pattern2' : 0}
-    TPA = {'idle' : 0.2, 'pattern1' : 1, 'pattern2' : 1}
+    FPA = {'idle' : len(IDLE), 'pattern1' : len(PATTERN1), 'pattern2' : len(PATTERN2)}
+    TPA = {'idle' : 0.2, 'pattern1' : 1, 'pattern2' : 1.5}
     SPEED = {'idle' : 100, 'pattern1' : 200, 'pattern2' : 0}    
 
     CLIP_SIZE = 64
@@ -109,7 +118,13 @@ class BabyPlum(Enemy):
     
     def draw(self):
         self.shadow.draw(self.x, self.y - 64)
-        self.image.clip_draw(self.CLIP[self.action][int(self.frame)][0], self.CLIP[self.action][int(self.frame)][1], self.CLIP_SIZE, self.CLIP_SIZE, self.x, self.y, self.draw_width, self.draw_height)
+        self.image.clip_draw(self.CLIP[self.action][int(self.frame)][0], # Crop X
+                            self.CLIP[self.action][int(self.frame)][1], # Crop Y
+                            self.CLIP_SIZE, self.CLIP_SIZE, # Crop Width, Height
+                            self.x + self.CLIP[self.action][int(self.frame)][4], # Position X 
+                            self.y + self.CLIP[self.action][int(self.frame)][5], # Position Y
+                            int(self.draw_width * self.CLIP[self.action][int(self.frame)][2]), # Scale X 
+                            int(self.draw_height * self.CLIP[self.action][int(self.frame)][3])) # Scale Y
         
         draw_rectangle(*self.get_bb())
         pass
@@ -147,7 +162,9 @@ class BabyPlum(Enemy):
         if self.idle_timer <= 0:
             self.idle_timer -= game_framework.frame_time
             self.idle_timer = 1
-            self.next_pattern = random.choice(self.ATTACK_LIST)
+            # self.next_pattern = random.choice(self.ATTACK_LIST)
+            self.next_pattern = 'pattern2'
+            
             self.direction = math.atan2(server.player.y - self.y, server.player.x - self.x)
             self.is_shoot = False
             return BehaviorTree.SUCCESS
@@ -178,9 +195,16 @@ class BabyPlum(Enemy):
     
     # pattern 2
     def pattern2(self):
-        # self.action = 'pattern2'
-        # self.speed = self.SPEED['pattern2']
-        pass
+        self.action = 'pattern2'
+        self.speed = self.SPEED['pattern2']
+        
+        if self.frame >= self.FPA[self.action] - 1:
+            self.frame = 0
+            self.direction = random.random() * 2 * math.pi
+            return BehaviorTree.SUCCESS
+        
+        
+        return BehaviorTree.RUNNING
     
     def shoot(self, way):
         # print('shoot')
